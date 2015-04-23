92cea6236861190a96efe4496e53753ac8e4d0a1
harshit19932703 <harshit19932703@gmail.com>
2015-04-23 17:19:00 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="keywords" content="Samsung Galaxy Grand Neo, Samsung Galaxy Grand Neo price, Android (4.2) phone, Samsung Galaxy Grand Neo specifications, Samsung Galaxy Grand Neo best price in india" /> 
+    <meta name="description" content="Get the Best Local & Online Prices for Samsung Galaxy Grand Neo as on April 13, 2015. Check Specifications, Compare Features & Set Product Alerts." /> 
+     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+ <link rel="shortcut icon" href="http://mdfl1.pricebaba.co/assets/images/favicons/favicon.ico" type="image/x-icon" />
+ <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+</script>
+<link rel="stylesheet" href="menucss/rmm-css/responsivemobilemenu.css" type="text/css"/>
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+<script type="text/javascript" src="menucss/rmm-js/responsivemobilemenu.js"></script>
+
+
+
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 70%;
+      margin: auto;
+  }
+  .style5 {font-family: Geneva, Arial, Helvetica, sans-serif;
+  color:Green;
+  font-size:20px;}
+  </style>
+
+  <style>
+      .style2 {font-family: Georgia, "Times New Roman", Times, serif; font-weight: bold; 
+      font-size:22px;}
+      .style3 {
+	font-size: 20px;
+	font-style: italic;
+	font-weight: bold;
+}
+.shadow1 {
+	margin: 20px;
+        background-color: grey; /* Needed for IEs */
+
+	-moz-box-shadow: 5px 5px 5px rgba(68,68,68,0.6);
+	-webkit-box-shadow: 5px 5px 5px rgba(68,68,68,0.6);
+	box-shadow: 5px 5px 5px rgba(68,68,68,0.6);
+
+	filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius=3,MakeShadow=true,ShadowOpacity=0.30);
+	-ms-filter: "progid:DXImageTransform.Microsoft.Blur(PixelRadius=3,MakeShadow=true,ShadowOpacity=0.30)";
+	zoom: 1;
+}
+.shadow1 .content {
+	position: relative; /* This protects the inner element from being blurred */
+	background-color: #DDD;
+        padding:10px;
+}
+  </style>
+
+
+
+
+
+
+
+        <title>MobileLook</title>
+       
+
+   <img src="images/pricebaba.png" class="img-responsive" align="left">
+
+
+    </head>
+    <body>
+                
+        
+                           <!--menu -->
+                 <div class="rmm">
+            <ul>
+                <li><a href=''>Popular Phones</a></li>
+                <li><a href=''>Apple</a></li>
+                <li><a href=''>Microsoft</a></li>
+                <li><a href=''>HTC</a></li>
+                <li><a href=''>Motorola</a></li>
+                <li><a href=''>Sony</a></li> 
+                 <li><a href=''>Budgetphones</a></li>
+                <li><a href=''>Camera phones</a></li> 
+                 <li><a href=''>AndroidPhones</a></li>
+                <li><a href=''>Latest</a></li> 
+            </ul>
+                <!-- menuend -->
+                <div>
+                <form>
+                    <select  id="sel1">
+        <option value="" selected disabled>Select City</option>
+        <option>Gurgaon</option>
+        <option>Noida</option>
+        <option>Mumbai</option>
+        <option>NewDelhi</option>
+      </select>
+                    
+   <input  type="search"  placeholder="Search your Product" />
+<a href="#" class="btn btn-danger " style="align:center"><span class="glyphicon glyphicon-search"></span> </a>
+
+                </form>
+                </div>
+                
+
+                
+                
+                <div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="images/p1.jpg" alt="" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="images/p2.jpg" alt="" width="460" height="345">
+      </div>
+    
+      <div class="item">
+        <img src="images/p3.jpg" alt="" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="images/p4.jpg" alt="" width="460" height="345">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+                
+                <br>             
+                <div class="shadow1">
+                       <div class="content">
+                     <p><span class="style2">Samsung Galaxy Grand Neo </span><span>Price </span></p>
+                     <p class="style5"><span style="color:red">Rs</span> 7,990-13,990/- </p>
+                     <p><span class="style3">Status</span>:<span class="style4"><button type="button" class="btn btn-success">Available</button></span></p>
+                       
+                     <a href="loc.php"><button type="button" class="btn btn-primary active">Buy From Local shops</button></a>
+                         <a href="onl.php"><button type="button" class="btn btn-danger">Buy ONLINE</button></a>
+                      
+                   
+<br>
+<p> <i>*Android 4.2 </i></p>
+<p> <i>*Camera:5px </i></p>
+<p><a href="spec.php" style="color:#4aeeee">See Full Specifiactions</a></p>
+                       </div>
+                </div>
+                <div align="center">
+               <a href="loc.php"><button type="button" class="btn btn-info">Check Local Prices</button></a>
+               <a href="onl.php"><button type="button" class="btn btn-success">Compare Online</button></a>
+                </div>
+          
+                <div class="shadow1">
+                    <div class="content" align="center"><i>Look For SimilarMobiles</i></div>
+                </div>
+      
+                <div style="padding:10px">              
+     <a href=""><img src="images/s1.jpg" height="150" width="100"> </a>
+   <a href=""><img src="images/s2.jpg" height="150" width="100"> </a> 
+    <a href=""><img src="images/s3.jpg" height="150" width="100"> </a> 
+    <a href=""> <img src="images/s4.jpg" height="150" width="100">  </a>
+   <a href=""></div>
+                
+                
+         <footer>  <a href="http://www.pricebaba.com"><button type="button" class="btn btn-primary btn-block">Visit our Website!</button></a></footer>
+            
+    </body>
+</html>
